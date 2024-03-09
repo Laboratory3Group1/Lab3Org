cb5ed5577b355cfa9b286851913ec29c00938e81
azraqt <altoverosdarren13@gmail.com>
2024-03-09 09:00:48 -0800

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,10 +4,8 @@ $username = "user";
 $password = "";
 $database = "dbTaskManagement";
 
-// Create connection
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
