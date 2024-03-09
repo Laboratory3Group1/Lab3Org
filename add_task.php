64a49d48b65161d047089777b9bb51dc1c7733e6
mikay123exd <abriammichaelajoan36@gmail.com>
2024-03-09 08:07:15 -0800

    Create add_task.php

---
+++ add_task.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $task_name = $_POST['task_name'];
+    $task_date = $_POST['task_date'];
+    $task_status = $_POST['task_status'];
+
+    $sql = "INSERT INTO tasks (taskname, date, status) VALUES ('$task_name', '$task_date', '$task_status')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New task added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+    header("Location: index.html");
+}
+?>
