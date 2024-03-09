2a94102c3cfa115f7d5bc56e95162035d0f77c04
azraqt <altoverosdarren13@gmail.com>
2024-03-09 09:39:44 -0800

    Update display_task.php

--- display_task.php
+++ display_task.php
@@ -6,7 +6,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row['taskname'] . " - " . $row['date'] . " - " . $row['status'] . " <a href='update_task.php?id=" . $row['id'] . "'>Edit</a> <a href='delete_task.php?id=" . $row['id'] . "'>Delete</a></li>";
+        echo "<li>" . $row['taskname'] . " - " . $row['date'] . " - " . $row['status'] . " <a href='update.php?id=" . $row['id'] . "'>Edit</a> <a href='delete_task.php?id=" . $row['id'] . "'>Delete</a></li>";
     }
 } else {
     echo "No tasks found";
