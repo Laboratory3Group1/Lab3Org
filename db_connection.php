6532ee0616383ab77210940ab7d6df58fee2ba07
Karkiesan12 <Abucalxyrus73@gmail.com>
2024-03-09 08:01:38 -0800

    db connection

---
+++ db_connection.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$username = "user";
+$password = "";
+$database = "dbTaskManagement";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
