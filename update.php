2a15309cd133ace8872ca6e88b3c01ef4a19a704
azraqt <altoverosdarren13@gmail.com>
2024-03-09 09:42:14 -0800

    Update update.php

--- update.php
+++ update.php
@@ -3,9 +3,9 @@ include 'db_connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $task_id = $_GET['id'];
-    $task_name = $_POST['task_name'];
-    $task_date = $_POST['task_date'];
-    $task_status = $_POST['task_status'];
+    $task_name = $_POST['taskname'];
+    $task_date = $_POST['date'];
+    $task_status = $_POST['status'];
 
     $sql = "UPDATE tasks SET taskname='$task_name', date='$task_date', status='$task_status' WHERE id=$task_id";
 
