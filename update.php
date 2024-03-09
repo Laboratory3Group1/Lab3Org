5f0d33fe552c0cf4264d7e098f49ad107a46b70b
CarlosBalmes <balmescarlos19@gmail.com>
2024-03-09 08:15:05 -0800

    Create update.php

---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $task_id = $_GET['id'];
+    $task_name = $_POST['task_name'];
+    $task_date = $_POST['task_date'];
+    $task_status = $_POST['task_status'];
+
+    $sql = "UPDATE tasks SET taskname='$task_name', date='$task_date', status='$task_status' WHERE id=$task_id";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Task updated successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+    header("Location: index.html");
+}
